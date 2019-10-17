330e363afd0c5ec8225577473e775d72f0280c0f
zopr86342 <55218489+zopr86342@users.noreply.github.com>
2019-10-17 12:11:41 +0800

    作業

---
+++ UnityProject/Assets/player.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    public Animator Chicken;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if (Input.GetKeyDown(KeyCode.E))
+        {
+            Chicken.SetTrigger("吃飯觸發器");
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            Chicken.SetBool("跑步開關",true);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            Chicken.SetBool("跑步開關",false);
+        }
+    }
+}
