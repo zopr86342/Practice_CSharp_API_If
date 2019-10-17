6a41ac7eaa8628c3b4ff9576d0eeaf4744fe486c
KID <50666207+KID421@users.noreply.github.com>
2019-10-17 11:18:41 +0800

    完成

---
+++ UnityProject/Assets/Chicken.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    public Animator chicken;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            chicken.SetTrigger("吃飯觸發器");
+        }
+
+        chicken.SetBool("跑步開關", Input.GetKey(KeyCode.R));
+    }
+}
